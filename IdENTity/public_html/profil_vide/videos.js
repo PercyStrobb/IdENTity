6f22803d201a74b05774654f4a82931f90bb39ef
brucebaumann <bruce.baumann@laposte.net>
2017-09-28 17:03:33 +0200

    Ajout du mail de ldelrous et bonton de victoire

--- IdENTity/public_html/profil_vide/videos.js
+++ IdENTity/public_html/profil_vide/videos.js
@@ -23,10 +23,12 @@ if (nom==="ldelrous"){
     if (pagevisitee==="Accueil"){
         document.write('<div style="position:fixed;right:20px;bottom:20px"><video width="320" height="260" autoplay controls><source src="videos/menace.ogg" type="video/ogg"></video></div>');
     }
+    if (pagevisitee==="Envoyes"){
+        document.write('<div><input type="button" value="!" onclick="indice3(this)" style="position:absolute;top:335px;left:1070px;background-color:red;border-color:red"></div>');
+    }
 }
 
 function indice1(el){
-    console.log("C'est un indice, un indice COLOSSAL !");
     var vid1 = document.createElement("video");
     vid1.width = '320';
     vid1.height = '260';
@@ -40,7 +42,6 @@ function indice1(el){
 }
 
 function indice2(el){
-    console.log("C'est un indice, un indice COLOSSAL !");
     var vid2 = document.createElement("video");
     vid2.width = '320';
     vid2.height = '260';
@@ -51,4 +52,9 @@ function indice2(el){
     src.type = 'video/ogg';
     vid2.appendChild(src);
     document.getElementById('dis2').appendChild(vid2);
+}
+
+function indice3(el){
+    console.log("C'est un indice, un indice COLOSSAL !");
+    alert("Bien joué, hacker. Tu as trouvé le coupable et ramener la paix à l'INU Champollion. Tu peux être fier de toi !");
 }
